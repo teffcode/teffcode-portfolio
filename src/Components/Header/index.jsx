c4b5791858ee92ff92b6795b5a1d8228992096c1
Estefany Aguilar <estefanyaguilar@LHMEDP67HHKL4LP.broadband>
2023-06-11 20:58:11 +0100

    add: blur and lazy load to header image

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,8 +1,22 @@
+import { useState, useRef, useEffect } from 'react'
 import { NavLink } from 'react-router-dom'
+import { LazyLoadImage, LazyLoadComponent } from 'react-lazy-load-image-component'
+import { Blurhash } from 'react-blurhash'
 import Presentation from '../../Assets/Presentation.png'
+import 'react-lazy-load-image-component/src/effects/blur.css'
 import './index.css'
 
 const Header = () => {
+  const [presentationLoaded, setPresentationLoaded] = useState([])
+
+  const handlePresentationLoad = (index) => {
+    setPresentationLoaded((prevLoaded) => {
+      const loaded = [...prevLoaded]
+      loaded[index] = true
+      return loaded
+    })
+  }
+
   return (
     <>
       <nav className='navbar'>
@@ -18,9 +32,18 @@ const Header = () => {
           </NavLink>
         </div>
       </nav>
-      <figure className='presentation'>
-        <img src={Presentation} alt="Presentation: Hi, I'm Estefany Aguilar. Frontend Developer and UX Designer." className='presentation__image' />
-      </figure>
+      <LazyLoadComponent>
+        <figure className='presentation__container'>
+          <Blurhash
+            hash='U45E]X8^Dh?cV?jYbIkD00?w-;8^t8azaej@'
+            className='presentation__blur' />
+          <LazyLoadImage
+            alt="Presentation: Hi, I'm Estefany Aguilar. Frontend Developer and UX Designer."
+            src={Presentation}
+            afterLoad={() => handlePresentationLoad(1)}
+            className={presentationLoaded[1] ? 'presentation__lazy-load visible' : 'presentation__lazy-load non-visible'} />
+        </figure>
+      </LazyLoadComponent>
     </>
   )
 }
